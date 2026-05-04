7bb23ebae961a282accf7eddee3d4c91a2196525
Przemyslaw Rachwlal <przemyslaw.rachwal@gmail.com>
2026-05-04 20:04:36 +0200

    Phase 01: add SREG bit-mask test for each flag

--- tests/EmulatorAVR.Core.Tests/Cpu/StatusRegisterTests.cs
+++ tests/EmulatorAVR.Core.Tests/Cpu/StatusRegisterTests.cs
@@ -29,6 +29,29 @@ public class StatusRegisterTests
     }
 
     [TestMethod]
+    public void EachFlagMapsToCorrectBit()
+    {
+        var flags = new (string name, Action<StatusRegister, bool> setter, Func<StatusRegister, bool> getter, byte mask)[]
+        {
+            ("I", (s, v) => s.I = v, s => s.I, 0x80),
+            ("T", (s, v) => s.T = v, s => s.T, 0x40),
+            ("H", (s, v) => s.H = v, s => s.H, 0x20),
+            ("S", (s, v) => s.S = v, s => s.S, 0x10),
+            ("V", (s, v) => s.V = v, s => s.V, 0x08),
+            ("N", (s, v) => s.N = v, s => s.N, 0x04),
+            ("Z", (s, v) => s.Z = v, s => s.Z, 0x02),
+            ("C", (s, v) => s.C = v, s => s.C, 0x01),
+        };
+
+        foreach (var (name, setter, getter, mask) in flags)
+        {
+            var sreg = new StatusRegister();
+            setter(sreg, true);
+            sreg.Value.Should().Be(mask, $"setting {name} should produce 0x{mask:X2}");
+        }
+    }
+
+    [TestMethod]
     public void Z_SetsBit1Only()
     {
         var sreg = new StatusRegister();
